50a9bc7a126c30a8293a566b07d602c49030014b
Kyle Marek-Spartz <kyle.marekspartz@govdelivery.com>
2017-04-22 13:26:29 -0500

    Split on one or more spaces

--- tests/test_cron_line.py
+++ tests/test_cron_line.py
@@ -5,8 +5,8 @@ from crontab_parser.cron_line import CronLine
 
 class TestCronLine(unittest.TestCase):
     def test_splits_into_fields(self) -> None:
-        line = "* * * * * command to run"
-        split_line = line.split(" ")
+        line = "*  *   *  *  * command to run"
+        split_line = line.split()
         minute, hour, day_of_month, month, day_of_week, *command = split_line
         cl = CronLine(line)
         self.assertEqual(minute, cl.minute)
