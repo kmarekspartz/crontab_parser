843c0edeb13946e8fed7801f0c6deb9dca7c076f
Kyle Marek-Spartz <kyle.marekspartz@govdelivery.com>
2017-04-22 13:23:01 -0500

    Split the cronline

--- crontab_parser/cron_line.py
+++ crontab_parser/cron_line.py
@@ -1,3 +1,10 @@
 class CronLine(object):
     def __init__(self, line: str) -> None:
-        pass
+        split_line = line.split(" ")
+        minute, hour, day_of_month, month, day_of_week, *command = split_line
+        self.minute = minute
+        self.hour = hour
+        self.day_of_month = day_of_month
+        self.month = month
+        self.day_of_week = day_of_week
+        self.command = command
--- tests/test_cron_line.py
+++ tests/test_cron_line.py
@@ -4,8 +4,17 @@ from crontab_parser.cron_line import CronLine
 
 
 class TestCronLine(unittest.TestCase):
-    def test_can_be_constructed(self) -> None:
-        CronLine("")
+    def test_splits_into_fields(self) -> None:
+        line = "* * * * * command to run"
+        split_line = line.split(" ")
+        minute, hour, day_of_month, month, day_of_week, *command = split_line
+        cl = CronLine(line)
+        self.assertEqual(minute, cl.minute)
+        self.assertEqual(hour, cl.hour)
+        self.assertEqual(day_of_month, cl.day_of_month)
+        self.assertEqual(month, cl.month)
+        self.assertEqual(day_of_week, cl.day_of_week)
+        self.assertEqual(command, cl.command)
 
 
 if __name__ == '__main__':
