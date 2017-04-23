ac1bcc30f3b5c2673af045045e433ef6518f27c5
Kyle Marek-Spartz <kyle.marekspartz@govdelivery.com>
2017-04-22 20:48:21 -0500

    All combinations of *

--- crontab_parser/cron_line.py
+++ crontab_parser/cron_line.py
@@ -16,22 +16,15 @@ class CronLine(object):
     def describe_command(self) -> str:
         return ' '.join(self.command)
 
-    time_descriptions = {
-        '*': 'every',
-        '1': 'the first',
-        '45': 'not a good approach',
-        '23': 'not at all',
-        '0': 'zero points'
-    }
-
-    def describe_time_(self, field, unit) -> str:
-        return f"{self.time_descriptions[field]} {unit}"
-
     def describe_minute(self) -> str:
-        return self.describe_time_(self.minute, 'minute')
+        if self.minute == '*':
+            return 'every minute'
+        return 'minute ' + self.minute
 
     def describe_hour(self) -> str:
-        return self.describe_time_(self.hour, 'hour')
+        if self.hour == '*':
+            return 'every hour'
+        return 'hour ' + self.hour
 
     def describe_month(self) -> str:
         try:
@@ -55,15 +48,19 @@ class CronLine(object):
         ])
 
     def describe_date(self) -> str:
+        if self.day_of_month == '*' and self.month == '*':
+            return ''
+        if self.day_of_month == '*':
+            return 'in ' + self.describe_month()
         if self.month == '*':
             return ''.join([
                 'day ',
                 self.day_of_month,
-                ' of every month'
+                ' every month'
             ])
         return ''.join([
             self.describe_month(),
-            " ",
+            ' ',
             self.day_of_month
         ])
 
@@ -71,43 +68,47 @@ class CronLine(object):
         if self.is_every_day():
             return ''
         if self.day_of_month == '*' and self.month == '*':
-            return self.describe_day_of_week()
+            return ' on ' + self.describe_day_of_week()
+        if self.day_of_week == '*' and self.day_of_month == '*':
+            return ' ' + self.describe_date()
+        if self.day_of_week == '*':
+            return ' on ' + self.describe_date()
         if self.day_of_month == '*':
             return ''.join([
-                "every ",
+                ' on every ',
                 self.describe_day_of_week(),
-                " in ",
+                ' in ',
                 self.describe_month()
             ])
         if self.day_of_week == '*':
-            return self.describe_date()
+            return ' on ' + self.describe_date()
         return ''.join([
+            ' on ',
             self.describe_day_of_week(),
-            ", ",
+            ', ',
             self.describe_date()
         ])
 
     def describe_time(self) -> str:
         if self.minute == '*' and self.hour == '*':
-            return self.describe_minute()
+            return 'at ' + self.describe_minute()
         if self.minute == '*' or self.hour == '*':
             return ''.join([
-               "on ",
+               'at ',
                self.describe_minute(),
-               " of ",
+               ' ',
                self.describe_hour()
             ])
-        return "at " + time.strftime(
-            "%H:%M",
+        return 'at ' + time.strftime(
+            '%H:%M',
             (0, 1, 2, int(self.hour), int(self.minute), 5, 6, 7, 8)
         )
 
     def describe(self) -> str:
         return ''.join([
-            "Run `",
+            'Run `',
             self.describe_command(),
-            "` ",
+            '` ',
             self.describe_time(),
-            '' if self.is_every_day() else " ",
             self.describe_days_and_months()
         ])
